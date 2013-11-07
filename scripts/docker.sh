a3f31579f7b67f880902531d00c75cc1835fa79a
Darron Froese <darron@froese.org>
2013-11-06 21:00:00 -0700

    Updating for current instructions and naming.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,8 +2,8 @@
 
 # Add the Docker repository to your apt sources list.
 sudo apt-get install curl
-sudo sh -c "curl http://get.docker.io/gpg | apt-key add -"
-sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
 # Update your sources
 sudo apt-get update
